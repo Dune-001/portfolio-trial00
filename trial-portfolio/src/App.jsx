4b020ff245b9d29f46d08d0721534dbc29e888d8
Dune-001 <daggerone24@gmail.com>
2025-09-24 13:18:12 +0300

    starting the project

---
+++ trial-portfolio/src/App.jsx
@@ -0,0 +1,22 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      {/* testing if tailwindcss works
+      <div className='min-h-screen bg-blue-100 flex items-center justify-center'>
+        <h1 className='text-4xl font-bold text-blue-600'>Tailwind is working! 🎉</h1>
+      </div>*/}
+      <div className="min-h-screen bg-gray-50">
+        <h1 className="text-4xl font-bold text-center py-8">My Portfolio</h1>
+        <p className="text-center">Welcome to my portfolio website!</p>
+      </div>
+    </>
+  )
+}
+export default App
