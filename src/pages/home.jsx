5667fbd7fe4f8a76443220835a77d21886be3b2e
Dune-001 <daggerone24@gmail.com>
2025-09-27 17:00:17 +0300

    made the pages for each section

---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MC from '../components/MC';
+import About from '../components/About';
+
+const Home = () => {
+    return (
+        <div>
+            <MC />
+            <About />
+        </div>
+    );
+};
+
+export default Home;
